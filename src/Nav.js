933ab5deefa35424572fd9d2c9477ed066218818
shapirodaniel <shapirodanieladam@gmail.com>
2021-12-04 16:22:59 -0600

    fix: nav link keys

--- src/Nav.js
+++ src/Nav.js
@@ -3,17 +3,16 @@ import { NavLink } from "react-router-dom";
 import "./Nav.css";
 
 const navLinks = [
-  { id: 1, name: "Home", to: "/" },
-  { id: 1, name: "Feed", to: "/feed" },
+  { name: "Home", to: "/" },
+  { name: "Feed", to: "/feed" },
 ];
 
 export default function Nav() {
   return (
     <section className="navContainer">
-      {navLinks.map(({ id, name, to }) => (
-        <NavLink exact activeClassName="active" to={to}>
+      {navLinks.map(({ name, to }, idx) => (
+        <NavLink key={idx} exact activeClassName="active" to={to}>
           <div
-            key={id}
             style={{
               paddingRight: "1.5em",
             }}
